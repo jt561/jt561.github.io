410f4ee39a6691799b4d1e838912018ec5e6cedc
Joshua Tetteh <jt561@kent.ac.uk>
2020-12-20 00:31:04 +0000

    fixed snake css

--- play-snake/sketch.js
+++ play-snake/sketch.js
@@ -13,11 +13,11 @@ var food;
 // called once
 function setup() {
   // create board/canvas
-  let deviceWidth = window.screen.width-12;
-  let deviceHeight = window.screen.height-230;
+  let deviceWidth = window.screen.width;
+  let deviceHeight = window.screen.height - ((window.matchMedia("(max-width: 768px)").matches) ? 280:300);
   let documentWidth = $(document).width() - 5;
   let documentHeight = $(document).height() - 100;
-  createCanvas(documentWidth, documentHeight);
+  createCanvas(deviceWidth, deviceHeight);
   // assign scale
   scl = 10;
   // assign/create new snake and food object with the same scale
