7ac39f882392e017c8d6d071f534e8c0b02d6607
Joshua Tetteh <jt561@kent.ac.uk>
2020-12-30 22:06:18 +0000

    quick sort visuals now works

--- visualize-sorting/quick-sort.js
+++ visualize-sorting/quick-sort.js
@@ -10,11 +10,81 @@ var QuickSort = function(arr)
 	this.i = 0;
 	this.j = 0;
 	this.done = false;
+	// start and end index, initialise with initial array
+	this.startInd = 0;
+	this.endInd = arr.length - 1;
+	// auxiliary stack/array
+	this.stack = [];
+	// push start and end index onto stack
+	this.stack.push(this.startInd);
+	this.stack.push(this.endInd);
 
 	// actual sort
 	this.sort = function()
 	{
-		printArrayAll(arr, this.j, this.j+1, this.j, this.done);
+		// whilst there are elements in the stack
+		if (this.stack.length > 0 )
+		{
+			// set start and end index
+			this.endInd = this.stack.pop();
+			this.startInd = this.stack.pop();
+			// partiiton
+			let partiitonInd = this.partition(arr, this.startInd, this.endInd);
+			// push left side of pivot to the stack
+			if (partiitonInd - 1 > this.startInd)
+			{
+        this.stack.push(this.startInd);
+        this.stack.push(partiitonInd - 1);
+      }
+			// push right side of pivot to the stack
+			if (partiitonInd + 1 < this.endInd)
+			{
+        this.stack.push(partiitonInd + 1);
+        this.stack.push(this.endInd);
+      }
+			// for visuals
+			this.i = partiitonInd;
+			this.j = this.endInd;
+		}
+		// end of outer loop
+		else
+		{
+				this.done = true;
+		}
+		printArrayAll(arr, this.j, this.i, this.startInd, this.done);
+	}
+
+	// partitioning/sorting
+	this.partition = function(arr, startInd, endInd)
+	{
+		// pivot index/partition index
+		let pivotInd = startInd;
+		let pivotValue = arr[endInd];
+		// get all the small elements to the left of pivot index
+		for (let i = startInd; i < endInd; i++)
+		{
+			if (arr[i] <= pivotValue)
+			{
+				// swap
+				let temp = arr[pivotInd];
+				arr[pivotInd] = arr[i];
+				arr[i] = temp;
+				// move pivot index forward
+				pivotInd++;
+
+				// update the global swap count
+				moreStats['swaps']++;
+			}
+			// update the global comparisons count
+			moreStats['comparisons']++;
+		}
+		// put the pivot value from end to its new position
+		// swap
+		let temp = arr[endInd];
+		arr[endInd] = arr[pivotInd];
+		arr[pivotInd] = temp;
+		// return pivot index
+		return pivotInd;
 	}
 }
 /* End of quick sort algorithm */
