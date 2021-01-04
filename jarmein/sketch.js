7a63182be88be27da8641eaafb98225421730450
Joshua Tetteh <jt561@kent.ac.uk>
2021-01-04 18:19:20 +0000

    added jarmein(voice assisstant) folder

---
+++ jarmein/sketch.js
@@ -0,0 +1,22 @@
+/**
+ * Main board
+ */
+
+/* Global variables */
+
+function preload()
+{
+
+}
+
+// called once at start
+function setup() {
+
+}
+
+// draw loop
+function draw() {
+
+}
+
+/* End of main board */
