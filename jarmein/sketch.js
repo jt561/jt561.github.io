8db1188681ed60d24635563650c3523d0feb89c6
Joshua Tetteh <jt561@kent.ac.uk>
2021-01-04 20:52:56 +0000

    start text is done. started working on canvas

--- jarmein/sketch.js
+++ jarmein/sketch.js
@@ -11,12 +11,12 @@ function preload()
 
 // called once at start
 function setup() {
-
+	createCanvas(200,200);
 }
 
 // draw loop
 function draw() {
-
+	background("white");
 }
 
 /* End of main board */
